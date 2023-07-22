603d5fb59d6d2be3d847b22bfca98d5341556745
Jean-Philippe Rivard <jprivard@petalmd.com>
2023-07-22 15:41:04 -0400

    Move links to Toolbar

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
+    MatToolbarModule, MatButtonModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
